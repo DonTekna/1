73df208f02dff202024e45f67dcb173edd49c4d3
Don Tekna Pathiraja <108195894+DonTekna@users.noreply.github.com>
2025-08-27 22:57:26 +0530

    My utils function

---
+++ class1.java
@@ -0,0 +1,9 @@
+public class MyUtils {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = addNumbers(5, 10);
+        System.out.println("Result: " + result); // Output: Result: 15
+    }
+}
